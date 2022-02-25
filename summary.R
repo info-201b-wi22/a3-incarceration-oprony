a99e96cff66c7502d73f673fec675d4e494468b3
oprony <oprony@uw.edu>
2022-02-25 10:00:42 -0800

    [starting the code/report]

---
+++ summary.R
@@ -0,0 +1,7 @@
+library(dplyr)
+library(ggplot2)
+library(tidyr)
+library(scales)
+
+data <- read.csv('https://raw.githubusercontent.com/vera-institute/incarceration-trends/master/incarceration_trends.csv')
+

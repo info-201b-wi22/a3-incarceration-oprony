8c2678dfec556adc0b71e09201f888bf8dcb6696
oprony <oprony@uw.edu>
2022-02-25 12:33:54 -0800

    [more bug fixing]

--- summary.R
+++ summary.R
@@ -28,19 +28,17 @@ pop_over_time_plot
 
 
 #Black vs White Pop Bar Chart
-total_white <- full_totals %>% pull(sum(white_jail_pop))
-total_black <- full_totals %>% pull(sum(black_jail_pop))
 
-black_v_white_pop_plot <- ggplot(data = total_black_white) + 
-  geom_bar(mapping = aes(x = "total_white"), color="Red") +
-  geom_bar(mapping = aes(x = "total_black"), color="Blue") +
+black_v_white_pop_plot <- ggplot(data = full_totals) + 
+  geom_bar(mapping = aes(x = "white_jail_pop"), color="Red") +
+  geom_bar(mapping = aes(x = "black_jail_pop"), color="Blue") +
   labs(title = "Comparison of Black vs White Jail Population", x = 'Race', y = "Population")
 black_v_white_pop_plot
 
 #map data of black pop
 
 state_data <- data %>% select("year", "state", "total_jail_pop", "black_jail_pop")
-state_data[is.na(state_data)] <- 0
+
 
 state_data <- state_data %>% group_by(state) %>% summarize(percent_black = (sum(black_jail_pop) / sum(total_jail_pop)) * 100)
 
@@ -53,7 +51,7 @@ state_data <- state_data %>% mutate(region = tolower(state.name))
 
 state_shape <- left_join(state_shape, state_data)
 
-state_plot <- ggplot(state_shape) + geom_polygon(mapping = aes(x=long, y=lat, group = group, fill = percent_black_white)) + scale_fill_continuous(low = 'black', high = 'red') + coord_map() + 
+state_plot <- ggplot(state_shape) + geom_polygon(mapping = aes(x=long, y=lat, group = group, fill = percent_black)) + scale_fill_continuous(low = 'black', high = 'red') + coord_map() + 
   labs(title = "Percentage of black per state")
 
 state_plot
